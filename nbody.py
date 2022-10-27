13cf9c5c6efbdaee4cb24ce9d1110d5d2eb67a08
Qiwei Shen <shenqiweixy@163.com>
2022-10-28 00:42:28 +0200

    Changed two code which be wrongly modified during add code to generate csv file.

--- nbody.py
+++ nbody.py
@@ -134,7 +134,7 @@ def main(n, ref="sun",write_decision=True):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 3:
+    if len(sys.argv) >= 2:
         main(int(sys.argv[1]),write_decision=bool(sys.argv[2]))
         # Below code was typed by shen qiwei
         with open("python_output.csv", "w", newline="") as file:
